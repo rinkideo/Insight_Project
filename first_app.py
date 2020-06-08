6b99d6aa43b1bf96fecdcbde4e321a315f504421
rinkideo <rinkideo@gmail.com>
2020-06-07 22:57:25 -0400

    app

--- first_app.py
+++ first_app.py
@@ -11,34 +11,119 @@ st.write("Here's our first attempt predict the risk of NICU admission")
 
 @st.cache
 def load_data():
-    df = pd.read_csv("CSV2018Head2.csv")
+    df = pd.read_csv('cleandata.csv',nrows=100)
     return df
 
 # Will only run once if already cached
 df = load_data()
 
-st.write(df.shape)
+st.write(df.shape[0])
+
+import pickle
+
+#
+# Load model
+## Save to file in the current working directory
+pkl_filename = "logreg_model.pkl"
+# Load from file
+with open(pkl_filename, 'rb') as file:
+    my_model = pickle.load(file)
+
+# Prediction
+def predict_nicu_ad(data):
+    result = my_model.predict(data)
+    return result
+
+def bmi_calc(wt,ht):
+    BMI = round((wt/2.2) / ((ht*0.0254)**2))
+    if BMI<18.5:
+        BMI_R = 1
+    if 18.5 < BMI< 25:
+        BMI_R = 2
+    if 25.0 <= BMI< 30.0:
+        BMI_R = 3
+    if 30 <= BMI< 35.0:
+        BMI_R = 4
+    if 35.0 <= BMI< 40:
+        BMI_R = 5
+    elif BMI >= 40.0:
+        BMI_R = 6
+    return BMI_R
+
+           
+Age = st.slider("Choose your age: ", min_value=10,   
+                   max_value=60, value=50, step=1)
+##input2a
+Pre_Preg_Weight = st.slider("Choose your weight(lb): ", min_value=50, max_value=350, value=300, step=1)
+    ##input2b
+Height = st.slider("Choose your height(inch): ", min_value=35,max_value=100, value=65, step=1)
+    ##Input2 calculated from 2a and 2b
+BMI_R = bmi_calc(Pre_Preg_Weight,Height)
+
+Weight = st.slider("Choose your weight in pounds: ", min_value=70,   
+                   max_value=350, value=280, step=1)
 
 
-option = st.selectbox(
-    'Expectant Mothers Age? Choose from 1: Under 15 Years, 2: 15-19 years, 3: 20-24 years, 4: 25-29 years , 5: 30-34 years, 6: 35-39 years, 7: 40-44 years, 8: 45-49 years, 9: 50-54 years' ,
-     df['MAGER9'].unique())
-
-'You selected: ', option
+bfacil_option = st.selectbox('Where do you like the birthplace to be? 1: Hospital, 2: Freestanding Birth Center, 3: Home (intended), 4: Home (not intended), 5: Home (unknown if intended), 6: Clinic / Doctor’s Office, 7: Other',
+                  (1,2,3,4,5,6,7))
 
 
+priorterm = st.text_input("No. of prior other terminations","Type Here")
+#priorterm = int(priorterm_option)
 
-option = st.selectbox(
-    'Expectant Mothers BMI? Choose from 1: Underweight <18.5, 2: Normal 18.5-24.9 , 3: Overweight 25.0-29.9, 4: Obesity I 35.0-34.9, 5: Obesity II 35.0-39.9, 6: Extreme Obesity III ≥ 40.0 ',
-     df['BMI_R'].unique())
+illb_r = st.text_input("Interval Since Last Live Birth(months)","Type Here")
+#illb_r = int(illb_option)
 
-'You selected: ', option
+previs = st.text_input("Number of prenatal visits done","Type Here")
+#previs = int(previs_option)
 
-option = st.selectbox(
-    'Expectant Mothers Risk Factors?' ,
-     df['BMI_R'].unique())
+rf_cesarean = st.text_input("Number of Previous Cesareans ","Type Here")
+#rf_cesarean = int(rf_ces_option)
 
-'You selected: ', option
+rf_option=st.selectbox('Count the Number of Pre-pregnancy risk factors out of following  exists/existed: Pre-pregnancy Diabetes, Gestational Diabetes, Pre-pregnancy Hypertension, Gestational Hypertension, Hypertension Eclampsia, Previous Preterm Birth, Infertility Treatment Used, Fertility Enhancing Drugs, Asst. Reproductive Technology',
+                      (0,1,2,3,4,5,6,7,8,9))
+cig_r = st.text_input('total Number of cigarettes (or packs) smoked from 3months prior to becoming pregnant till date ',"Type Here")
+
+#cig_r = int(cig_options)
+
+ip_option=st.selectbox('Count the Number of infections exists/existed: among gonorrhea; syphilis; chlamydia; hepatitis B; and hepatitis C ', (0,1,2,3,4,5))
 
 
-st.write('Probability of emergency is:')
+test_data = [Age,bfacil_option,priorterm,illb_r,previs,BMI_R,rf_cesarean,rf_option,cig_r,ip_option]
+
+
+COLUMN_NAMES = ['MAGER9', 'BFACIL', 'PRIORTERM', 'ILLB_R11', 'PREVIS_REC', 
+                'BMI_R','RF_CESARN', 'RF_Total', 'CIG_Total', 'IP_Total']
+test = pd.DataFrame(columns=COLUMN_NAMES,dtype='int')
+test.loc[0,:]=test_data
+df_test = test
+
+if st.button("Predict"):
+    result = predict_nicu_ad(df_test)
+    if result[0] == 0:
+        prediction = 'NO RISK OF NICU Admission'
+    else:
+        result[0] == 1
+        prediction = 'YES THERE IS RISK OF NICU Admission'
+    st.write(prediction)    
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
